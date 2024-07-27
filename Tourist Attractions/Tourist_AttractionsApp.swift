b4c57ecc3860d3aea15ecbab032cb3f92b10e5c1
Tin <93732406+Tin0709@users.noreply.github.com>
2024-07-27 15:53:28 +0700

    Initial Commit

---
+++ Tourist Attractions/Tourist_AttractionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tourist_AttractionsApp.swift
+//  Tourist Attractions
+//
+//  Created by Tin on 27/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Tourist_AttractionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
